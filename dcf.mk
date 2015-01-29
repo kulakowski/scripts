8c14c1edbc81e513b037ce271cfcbe2971f7e2dd
George Kulakowski <george@apporatable.com>
2015-01-29 14:19:03 -0800

    dcf Makefile

---
+++ dcf.mk
@@ -0,0 +1,43 @@
+release ?= no
+ifeq ($(release), no)
+release_flag :=
+else
+release_flag := --release
+endif
+xcode := ./setup-apportable-xcode.sh $(release_flag)
+arch := armv7a-neon
+testcl := ./tests/testcl.py --arch $(arch) $(release_flag)
+dcf := $(shell basename $(shell git rev-parse --show-toplevel))
+dt := bin/dt
+update_toolchain := update_toolchain
+branch :=
+
+.phony: *
+
+all: toolchain
+	@:
+
+xc: toolchain
+	$(xcode) $(filter-out $@,$(MAKECMDGOALS))
+
+sb: toolchain
+	$(xcode) --sbandroid $(filter-out $@,$(MAKECMDGOALS))
+
+clean:
+	$(xcode) --clean $(filter-out $@,$(MAKECMDGOALS))
+
+test: toolchain
+	$(testcl) $(filter-out $@,$(MAKECMDGOALS))
+
+toolchain: checkout
+	@$(dt) $(update_toolchain) $(filter-out $@,$(MAKECMDGOALS))
+
+checkout:
+ifdef branch
+	git checkout $(branch)
+else
+	@:
+endif
+
+%:
+	@:
