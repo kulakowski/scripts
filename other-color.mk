60a08b86a1078c3040caec7d79e54dec91e52235
George Kulakowski <kulakowski@google.com>
2016-07-14 15:55:47 -0700

    Rationalize paths in a jiri managed world

--- other-color.mk
+++ other-color.mk
@@ -1,22 +1,23 @@
-MAGENTA_SRC := /slice/magenta
 FUCHSIA_SRC := /slice/fuchsia
+MAGENTA_SRC := $(FUCHSIA_SRC)/magenta
+TOOLCHAIN_SRC := $(FUCHSIA_SRC)/third_party/gcc_none_toolchains
+QEMU_SRC := $(FUCHSIA_SRC)/third_party/qemu
+SDK_SRC := $(FUCHSIA_SRC)/buildtools/sdk/toolchains/
 
 CLANG := no
 ifneq (no,$(CLANG))
-FLAGS_CLANG := CLANG=1 FUCHSIA=1 ARCH_x86_64_TOOLCHAIN_PREFIX=$(MAGENTA_SRC)/sdk/toolchains/clang+llvm-x86_64-linux/bin/ LIBGCC=$(MAGENTA_SRC)/sdk/toolchains/clang+llvm-x86_64-linux/lib/clang/3.9.0/lib/fuchsia/libclang_rt.builtins-x86_64.a
-export PATH := $(PATH):$(MAGENTA_SRC)/sdk/toolchains/clang+llvm-x86_64-linux/bin
+FLAGS_CLANG := CLANG=1 FUCHSIA=1 ARCH_x86_64_TOOLCHAIN_PREFIX=$(SDK_SRC)/clang+llvm-x86_64-linux/bin/ LIBGCC=$(SDK_SRC)/clang+llvm-x86_64-linux/lib/clang/3.9.0/lib/fuchsia/libclang_rt.builtins-x86_64.a
+export PATH := $(PATH):$(SDK_SRC)/clang+llvm-x86_64-linux/bin
 else
 FLAGS_CLANG :=
 endif
 
-export PATH := $(PATH):$(MAGENTA_SRC)/toolchains/aarch64-elf-5.3.0-Linux-x86_64/bin
-export PATH := $(PATH):$(MAGENTA_SRC)/toolchains/arm-eabi-5.3.0-Linux-x86_64/bin
-export PATH := $(PATH):$(MAGENTA_SRC)/toolchains/i386-elf-5.3.0-Linux-x86_64/bin
-export PATH := $(PATH):$(MAGENTA_SRC)/toolchains/x86_64-elf-5.3.0-Linux-x86_64/bin
-export PATH := $(PATH):$(MAGENTA_SRC)/qemu/aarch64-softmmu
-export PATH := $(PATH):$(MAGENTA_SRC)/qemu/arm-softmmu
-export PATH := $(PATH):$(MAGENTA_SRC)/qemu/i386-softmmu
-export PATH := $(PATH):$(MAGENTA_SRC)/qemu/x86_64-softmmu
+export PATH := $(PATH):$(TOOLCHAIN_SRC)/aarch64-elf-5.3.0-Linux-x86_64/bin
+export PATH := $(PATH):$(TOOLCHAIN_SRC)/arm-eabi-5.3.0-Linux-x86_64/bin
+export PATH := $(PATH):$(TOOLCHAIN_SRC)/x86_64-elf-5.3.0-Linux-x86_64/bin
+export PATH := $(PATH):$(QEMU_SRC)/aarch64-softmmu
+export PATH := $(PATH):$(QEMU_SRC)/arm-softmmu
+export PATH := $(PATH):$(QEMU_SRC)/x86_64-softmmu
 
 export ENABLE_BUILD_LISTFILES := true
 export ENABLE_BUILD_SYSROOT := true
@@ -65,17 +66,24 @@ else
 FLAGS_U :=
 endif
 
+BOOTFS := no
+ifneq (no,$(BOOTFS))
+FLAGS_BOOTFS := -x ../$(BOOTFS)
+else
+FLAGS_BOOTFS :=
+endif
+
 all: buildall
 	@:
 
 clean:
-	@rm -rf $(MAGENTA_SRC)/magenta/build-*
+	@rm -rf $(MAGENTA_SRC)/build-*
 
 arm32:
 	@$(MAKE) -C magenta magenta-qemu-arm32
 
 run-arm32: arm32
-	@cd $(MAGENTA_SRC)/magenta && ./scripts/run-magenta-arm32 $(FLAGS_RELEASE) $(FLAGS_U) $(FLAGS_GDB)
+	@cd $(MAGENTA_SRC) && ./scripts/run-magenta-arm32 $(FLAGS_RELEASE) $(FLAGS_U) $(FLAGS_BOOTFS) $(FLAGS_GDB)
 
 debug-arm32:
 	@arm-eabi-gdb magenta/build-magenta-qemu-arm32/lk.elf
@@ -84,7 +92,7 @@ arm64:
 	@$(MAKE) -C magenta magenta-qemu-arm64
 
 run-arm64: arm64
-	@cd $(MAGENTA_SRC)/magenta && ./scripts/run-magenta-arm64 $(FLAGS_RELEASE) $(FLAGS_U) $(FLAGS_GDB)
+	@cd $(MAGENTA_SRC) && ./scripts/run-magenta-arm64 $(FLAGS_RELEASE) $(FLAGS_U) $(FLAGS_BOOTFS) $(FLAGS_GDB)
 
 debug-arm64:
 	@aarch64-elf-gdb magenta/build-magenta-qemu-arm64/lk.elf
@@ -93,22 +101,13 @@ x64:
 	@$(MAKE) -C magenta magenta-qemu-x86-64
 
 run-x64: x64
-	@cd $(MAGENTA_SRC)/magenta/ && ./scripts/run-magenta-x86-64 $(FLAGS_RELEASE) $(FLAGS_U) $(FLAGS_GDB)
-
-debug-x64:
-	@x86_64-elf-gdb magenta/build-magenta-qemu-x86-64/lk.elf
-
-pixel2:
-	@$(MAKE) -C magenta magenta-pc-uefi
-
-run-pixel2: pixel2 bootserver
-	$(MAGENTA_SRC)/magenta/tools/bootserver $(MAGENTA_SRC)/magenta/build-magenta-pc-uefi/lk.bin
+	@cd $(MAGENTA_SRC)/ && ./scripts/run-magenta-x86-64 $(FLAGS_RELEASE) $(FLAGS_U) $(FLAGS_BOOTFS) $(FLAGS_GDB)
 
 buildall:
-	cd $(MAGENTA_SRC)/magenta && env $(FLAGS_DEBUG) $(FLAGS_CLANG) ./scripts/buildall -r
+	cd $(MAGENTA_SRC) && env $(FLAGS_DEBUG) $(FLAGS_CLANG) ./scripts/buildall -r
 
-bootserver:
-	@$(MAKE) -C magenta/tools bootserver
+pc:
+	@$(MAKE) -C magenta magenta-pc-uefi
 
-bootimg-pixel2:
-	@cd $(FUCHSIA_SRC) && ./fuchsia-build fuchsia/bootimg/pixel2
+bootserver: pc
+	$(MAGENTA_SRC)/build-magenta-pc-uefi/tools/bootserver $(MAGENTA_SRC)/build-magenta-pc-uefi/magenta.bin $(BOOTFS)
