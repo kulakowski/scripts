8906bd3ee673a2fbda4b095ad776885453b531bc
George Kulakowski <kulakowski@google.com>
2017-09-12 15:32:09 -0700

    Rename magenta to zircon

--- fuchsia.mk
+++ fuchsia.mk
@@ -52,18 +52,18 @@ FUCHSIA_DIR := $(HOME)/fuchsia
 
 SCRIPTS_DIR := $(FUCHSIA_DIR)/scripts
 
-MAGENTA_DIR := $(FUCHSIA_DIR)/magenta
-MAGENTA_SCRIPTS_DIR := $(MAGENTA_DIR)/scripts
+ZIRCON_DIR := $(FUCHSIA_DIR)/zircon
+ZIRCON_SCRIPTS_DIR := $(ZIRCON_DIR)/scripts
 
 BUILDTOOLS_DIR := $(FUCHSIA_DIR)/buildtools
 QEMU_DIR := $(BUILDTOOLS_DIR)/qemu/bin
 GOMA_DIR := ~/goma
 
 FUCHSIA_OUT_DIR := $(FUCHSIA_DIR)/out
-MAGENTA_OUT_DIR := $(FUCHSIA_OUT_DIR)/build-magenta
-MAGENTA_ARM64_OUT_DIR := $(MAGENTA_OUT_DIR)/build-magenta-qemu-arm64$(BUILD_SUFFIX)
-MAGENTA_X64_OUT_DIR := $(MAGENTA_OUT_DIR)/build-magenta-pc-x86-64$(BUILD_SUFFIX)
-TOOLS_OUT_DIR := $(MAGENTA_OUT_DIR)/build-magenta-pc-x86-64$(BUILD_SUFFIX)/tools
+ZIRCON_OUT_DIR := $(FUCHSIA_OUT_DIR)/build-zircon
+ZIRCON_ARM64_OUT_DIR := $(ZIRCON_OUT_DIR)/build-zircon-qemu-arm64$(BUILD_SUFFIX)
+ZIRCON_X64_OUT_DIR := $(ZIRCON_OUT_DIR)/build-zircon-pc-x86-64$(BUILD_SUFFIX)
+TOOLS_OUT_DIR := $(ZIRCON_OUT_DIR)/build-zircon-pc-x86-64$(BUILD_SUFFIX)/tools
 
 FUCHSIA_OUT_PREFIX := $(FUCHSIA_OUT_DIR)/$(BUILD_NAME)
 
@@ -73,27 +73,27 @@ goma:
 
 
 arm64:
-	$(MAGENTA_SCRIPTS_DIR)/make-parallel -C $(MAGENTA_DIR) BUILDROOT=$(MAGENTA_OUT_DIR) magenta-qemu-arm64
+	$(ZIRCON_SCRIPTS_DIR)/make-parallel -C $(ZIRCON_DIR) BUILDROOT=$(ZIRCON_OUT_DIR) zircon-qemu-arm64
 
 x64:
-	$(MAGENTA_SCRIPTS_DIR)/make-parallel -C $(MAGENTA_DIR) BUILDROOT=$(MAGENTA_OUT_DIR) magenta-pc-x86-64
+	$(ZIRCON_SCRIPTS_DIR)/make-parallel -C $(ZIRCON_DIR) BUILDROOT=$(ZIRCON_OUT_DIR) zircon-pc-x86-64
 
 tools:
-	$(MAGENTA_SCRIPTS_DIR)/make-parallel -C $(MAGENTA_DIR) BUILDROOT=$(MAGENTA_OUT_DIR) tools
+	$(ZIRCON_SCRIPTS_DIR)/make-parallel -C $(ZIRCON_DIR) BUILDROOT=$(ZIRCON_OUT_DIR) tools
 
 
 run-arm64: arm64
-	$(MAGENTA_SCRIPTS_DIR)/run-magenta -o $(MAGENTA_ARM64_OUT_DIR) -a arm64 -q $(QEMU_DIR) $(KVM_FLAGS)
+	$(ZIRCON_SCRIPTS_DIR)/run-zircon -o $(ZIRCON_ARM64_OUT_DIR) -a arm64 -q $(QEMU_DIR) $(KVM_FLAGS)
 
 run-x64: x64
-	$(MAGENTA_SCRIPTS_DIR)/run-magenta -o $(MAGENTA_X64_OUT_DIR) -a x86-64 -q $(QEMU_DIR) $(KVM_FLAGS)
+	$(ZIRCON_SCRIPTS_DIR)/run-zircon -o $(ZIRCON_X64_OUT_DIR) -a x86-64 -q $(QEMU_DIR) $(KVM_FLAGS)
 
 
 sysroot-arm64: goma
-	$(SCRIPTS_DIR)/build-magenta.sh -t aarch64
+	$(SCRIPTS_DIR)/build-zircon.sh -t aarch64
 
 sysroot-x64: goma
-	$(SCRIPTS_DIR)/build-magenta.sh -t x86_64
+	$(SCRIPTS_DIR)/build-zircon.sh -t x86_64
 
 
 packages-arm64: sysroot-arm64
@@ -111,17 +111,17 @@ fuchsia-x64: packages-x64
 
 
 run-fuchsia-arm64: fuchsia-arm64
-	$(MAGENTA_SCRIPTS_DIR)/run-magenta -o $(MAGENTA_ARM64_OUT_DIR) -a arm64 -q $(QEMU_DIR) -x $(FUCHSIA_OUT_PREFIX)-aarch64/user.bootfs
+	$(ZIRCON_SCRIPTS_DIR)/run-zircon -o $(ZIRCON_ARM64_OUT_DIR) -a arm64 -q $(QEMU_DIR) -x $(FUCHSIA_OUT_PREFIX)-aarch64/user.bootfs
 
 run-fuchsia-x64: fuchsia-x64
-	$(MAGENTA_SCRIPTS_DIR)/run-magenta -o $(MAGENTA_X64_OUT_DIR) -a x86-64 -q $(QEMU_DIR) -x $(FUCHSIA_OUT_PREFIX)-x86-64/user.bootfs
+	$(ZIRCON_SCRIPTS_DIR)/run-zircon -o $(ZIRCON_X64_OUT_DIR) -a x86-64 -q $(QEMU_DIR) -x $(FUCHSIA_OUT_PREFIX)-x86-64/user.bootfs
 
 
 boot: x64 tools
-	$(TOOLS_OUT_DIR)/bootserver -1 $(MAGENTA_X64_OUT_DIR)/magenta.bin $(ALL_BOOT_ARGS)
+	$(TOOLS_OUT_DIR)/bootserver -1 $(ZIRCON_X64_OUT_DIR)/zircon.bin $(ALL_BOOT_ARGS)
 
 fuchsia-boot: fuchsia-x64 tools
-	$(TOOLS_OUT_DIR)/bootserver -1 $(MAGENTA_X64_OUT_DIR)/magenta.bin $(FUCHSIA_OUT_PREFIX)-x86-64/user.bootfs $(ALL_BOOT_ARGS)
+	$(TOOLS_OUT_DIR)/bootserver -1 $(ZIRCON_X64_OUT_DIR)/zircon.bin $(FUCHSIA_OUT_PREFIX)-x86-64/user.bootfs $(ALL_BOOT_ARGS)
 
 
 reboot: tools
